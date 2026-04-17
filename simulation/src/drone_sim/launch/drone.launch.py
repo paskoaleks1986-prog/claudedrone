452f2a082a5f12d756ae7be52917125b34b98c5d
alekspc <paskoaleks1986@gmail.com>
2026-04-17 19:36:06 +0100

    feat: add ros_gz_bridge to launch file

--- simulation/src/drone_sim/launch/drone.launch.py
+++ simulation/src/drone_sim/launch/drone.launch.py
@@ -1,9 +1,30 @@
+import os
 from launch import LaunchDescription
+from launch.actions import ExecuteProcess
 from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
 
 
 def generate_launch_description():
+
+    pkg = get_package_share_directory('drone_sim')
+
+    # Путь к миру
+    world = os.path.join(pkg, 'worlds', 'indoor_room.sdf')
+
+    # Путь к моделям
+    models = os.path.join(pkg, 'models')
+
     return LaunchDescription([
+
+        # Запускаем Gazebo
+        ExecuteProcess(
+            cmd=['gz', 'sim', '-r', world],
+            additional_env={
+                'GZ_SIM_RESOURCE_PATH': models
+            },
+            output='screen'
+        ),
 
         # Нода публикатор — мок датчиков
         Node(
@@ -13,11 +34,25 @@ def generate_launch_description():
             output='screen'
         ),
 
-        # Нода монитор — CRITICAL/HIGH/LOW
+        # Нода монитор
         Node(
             package='drone_sim',
             executable='sensor_monitor',
             name='sensor_monitor',
+            output='screen'
+        ),
+        Node(
+            package='ros_gz_bridge',
+            executable='parameter_bridge',
+            name='gz_bridge',
+            arguments=[
+                '/world/indoor_room/pose/info'
+                '@geometry_msgs/msg/PoseArray'
+                '[gz.msgs.Pose_V',
+                '/clock'
+                '@rosgraph_msgs/msg/Clock'
+                '[gz.msgs.Clock',
+            ],
             output='screen'
         ),
 
